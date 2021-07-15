71acc9dd5eda15289b86bb082c7d1d7e73732786
clarke <cf.lamb25@gmail.com>
2021-07-14 20:06:02 -0400

    branch

--- LibraryProject1/Program.cs
+++ LibraryProject1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LibraryProject1
 {
@@ -6,7 +8,98 @@ namespace LibraryProject1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // creating library book database
+            List<Book> bookList = new List<Book>();
+            bookList.Add(new Book()
+            {
+                Title = "Amari and the Night Brothers",
+                Author = "B.B. Alston",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "Children of Blood and Bone",
+                Author = "Tomi Adeyemi",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "Children of Virtue and Vengeance",
+                Author = "Tomi Adeyemi",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "The Coldest Winter Ever",
+                Author = "Sister Souljah",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "No Disrespect",
+                Author = "Sister Souljah",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "Firekeeper's Daughter",
+                Author = "Angeline Boulley",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "Etiquette and Espionage",
+                Author = "Gail Carriger",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "Soulless",
+                Author = "Gail Carriger",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "From Here to Timbuktu",
+                Author = "Milton J. Davis",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "Who Fears Death",
+                Author = "Nnedi Okorafor",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "Song of Blood and Stone",
+                Author = "L. Penelope",
+                Status = true,
+                DueDate = "Date.now"
+            });
+            bookList.Add(new Book()
+            {
+                Title = "Obsidian and Blood",
+                Author = "Aliette de Bodard",
+                Status = true,
+                DueDate = "Date.now"
+            });
+
+          
+            
         }
+
+       
     }
+
 }
